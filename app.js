4c109b9f9378461b1a843d84e76089964f1c137b
husnain-chemnitz <husnain.sarwar@s2020.tu-chemnitz.de>
2020-09-28 16:30:28 +0500

    next day commit3

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require("express");
+var express = require("expres");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.end("Hello World Husnain Manj in chemnitz Germany");
+  res.send("Hello World Husnain Manj in chemnitz Germany");
 });
 
 app.listen(4000);
